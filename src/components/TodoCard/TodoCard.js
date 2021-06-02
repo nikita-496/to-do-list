62e7d57f56e70ce8145b9a0cf6b464230cfac4cd
Nickita Shevkunov <shevkunovnikita97@gmail.com>
2021-06-02 08:16:11 +0500

    added task creation functionality

--- src/components/TodoCard/TodoCard.js
+++ src/components/TodoCard/TodoCard.js
@@ -9,35 +9,41 @@ export default class TodoCard extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            taskText: ""
+            taskText: "",
+            task: []
         }  
         //this.addTask = this.addTask.bind(this)
         this.updateText = this.updateText.bind(this)
+        this.createTask = this.createTask.bind(this)
+
+       console.log(this.state.taskText)
     }
 
     updateText (taskText) {
         this.setState({taskText})
     }
+    
+    createTask (nameTask) {
+        debugger
+        let itemArray = this.state.task
+        itemArray.unshift({
+            text: nameTask, 
+            key: Date.now()
+        }) 
+        debugger
+        this.setState({task: itemArray})
+    }
 
-    /*addTask (e) {
-        debugger
-        let a = this.state.task
-        debugger
-         a.unshift(e)
-        debugger
-        //debugger
-        this.setState({
-            task: a
-        })
-        debugger
+    /*deleteTask () {
+
     }*/
-   
+
 
     render() {
         const {taskText} = this.state
         return <div className = "TodoCard">
-                <AddToDo taskText={taskText} addTask={this.addTask} updateText ={this.updateText}/>
-                <ViewTodo />
+                <AddToDo taskText={taskText} createTask={this.createTask} updateText ={this.updateText}/>
+                <ViewTodo taskText={taskText}/>
                 <TodoNavigation />
             </div>
     }
--- src/components/TodoCard/TodoCardElements/AddTodo/AddTodo.js
+++ src/components/TodoCard/TodoCardElements/AddTodo/AddTodo.js
@@ -2,18 +2,18 @@ import React from "react"
 
 export const AddToDo = (props) => {
         debugger
-        const task = props.task
+        const taskText = props.taskText
         debugger
-        /*const handleTask = (e) => {
-          props.addTask(e.target.value)
-        }*/
         const handleText = (e) => {
           props.updateText(e.target.value)
         }
+        const handleTask = (e) => {
+          props.createTask(e.target.value)
+        }
         debugger
         return <div>
-                <input value={task} onChange={handleText} type="text" placeholder="Создать задачу"/>
-                <input type="submit" value="Добавить"/>
+                <input value={taskText} onChange={handleText} type="text" placeholder="Создать задачу"/>
+                <button type="button" value={taskText} onClick={handleTask}>Добавить</button>
                  <hr/>
             </div>
     
