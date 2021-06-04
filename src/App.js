2f1dc443bd15f821aad4d75fe3b1a2b01d5576f0
Nickita Shevkunov <shevkunovnikita97@gmail.com>
2021-06-04 12:57:27 +0500

    adding navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Header from './components/Header/Header';
-import TodoCard from './components/TodoCard/TodoCard';
+import TodoCard from './common/TodoCard/TodoCard';
 import Footer from './components/Footer/Footer';
+import Active from './components/Active/Active';
+import Complate from './components/Complated/Complated';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <TodoCard />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Route path="/" component={TodoCard}/>
+        <Route path="/active" component={Active}/>
+        <Route path="/complate" component={Complate}/>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
---
+++ src/components/Complated/Complated.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+const Complate = () => {
+  return (
+    <div >
+          <h1>Сomplate</h1>
+    </div>
+  );
+}
+export default Complate;
