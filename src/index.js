ff541535c493999d11e228497438732129d2b954
Nickita Shevkunov <shevkunovnikita97@gmail.com>
2021-06-23 10:23:32 +0500

    logic for defining completed tasks

--- src/index.js
+++ src/index.js
@@ -1,91 +1,182 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
 
-class TaskRow extends Component {
-  render() {
-    const {name} = this.props
+
+function TaskTable (props) {
+ function handleIsComplated (e, key = props.id) {
+  props.updataComplated(e.target.checked, key)
+  }
     return (
       <li>
-        <input type="checkbox"/>
-        {name}
-        <button>Удалить</button>
-        
-      </li>
+      <input type="checkbox" onClick={handleIsComplated}/>
+      {props.name}
+      <button>Удалить</button>
+    </li>
     )
   }
-}
 
-class TaskTable extends Component {
-  render() {
-    const {tasks} = this.props
-    const listTask = tasks.map((task) => {
-      return <TaskRow name={task.nameTask} key={task.id}/>
-    })
-    return (
-      <ul>
-        {listTask}
-      </ul>
-    )
-  }
-}
-class NavBar extends Component {
-  render() {
+function NavBar (props) {
     return (
       <div>
         <p>NavBar</p>
         
-        <a>Все</a>
-        <a>Активные</a>
-        <a>Завершенные</a>
+        <NavLink to="all">Все</NavLink>
+        <NavLink to="active">Активные</NavLink>
+        <NavLink to="complated">Завершенные</NavLink>
       </div>
+    )
+}
+
+function ComplateTaskTable (props) {
+  const listTask = props.tasks.map((task) => {
+    return <TaskTable name={task.name} key={task.key} id={task.key} updataComplated={props.updataComplated}/>
+  })
+  return(
+    <ul>
+      {listTask}
+    </ul>
+    
+  )
+}
+
+function ActiveTaskTAble (props) {
+return (
+  <ul>
+    Активные
+  </ul>
+)
+}
+
+class AllTask extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isComplated: false
+    }
+    this.updataComplated = this.updataComplated.bind(this)
+  }
+
+  updataComplated (isComplated, key) {
+    this.setState({
+      isComplated
+    })
+    this.props.updataTask(isComplated, key)
+  }
+
+  render () {
+    const listTask = this.props.tasks.map((task) => {
+      return <TaskTable name={task.name} key={task.key} id={task.key} updataComplated={this.updataComplated}/>
+    })
+    return(
+      <ul>
+        {listTask}
+      </ul>
+      
     )
   }
 }
-class SearchBar extends Component {
-  render() {
+
+function SearchBar (props) {
+  const nameTask = props.nameTask
+
+    function handleTaskName (e) {
+      props.createTask(e.target.value)
+    }
+
+    function handleTask (e) {
+      props.addTask(e.target.value)
+    }
     return (
       <div>
-        <input type="text" placeholder="введите задачу..."/>
-        <button type="button">Добавить</button>
+        <input type="text" placeholder="введите задачу..." value={nameTask} onChange={handleTaskName} />
+        <button type="button" value={nameTask} onClick={handleTask}>Добавить</button>
         <hr />
       </div>
-    )
-  }
+      )
 }
 
 class TodoCard extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      nameTask:"", 
+      tasks: []
+    }
+    this.createTask = this.createTask.bind(this)
+    this.addTask = this.addTask.bind(this)
+    this.updataTask = this.updataTask.bind(this)
+  }
+  createTask(nameTask) {
+    this.setState({
+      nameTask
+    })
+  }
+/*
+  updataComplated(isComplated, key) {
+    debugger
+    let taskArray = this.state.tasks
+    let complatedTask = taskArray.filter(task => task.key === key) 
+    console.log(complatedTask)
+  
+    this.setState({
+      isComplated
+    })
+    debugger
+  }
+  */
+  updataTask(isComplated, key) {
+   let n = this.state.tasks
+    let f = n.filter(n => n.key === key)
+    console.log (f)
+    let c = {
+      a:11,
+      b:12
+    }
+    c.a=13
+    console.log(c)
+    debugger
+  }
+
+  addTask(name, isComplated = false) {
+    let taskArray = this.state.tasks
+    if (name !== "") {
+      taskArray.unshift({
+        name,
+        key:Date.now(),
+        isComplated
+      })
+      this.setState({
+        nameTask: "",
+        tasks: taskArray
+      })
+    }
+  }
+
   render() {
-    const {tasks} = this.props
+    const {nameTask, tasks} = this.state
     return (
-      <div>
-        <SearchBar />
-        <TaskTable tasks={tasks} />
+        <BrowserRouter>
+        <SearchBar nameTask={nameTask} createTask={this.createTask} addTask={this.addTask}/>
+
+        <Route path="/all" render={(props)=> <AllTask  tasks={tasks} updataTask={this.updataTask}/>}/>
+      <Route path="/active"render={(props)=> <ActiveTaskTAble  tasks={tasks} />}/>
+      <Route path="/complated" render={(props)=> <ComplateTaskTable  tasks={tasks}/>}/>
+
         <NavBar />
-      </div>
+        </BrowserRouter>
+
     )
   }
 }
 
-
-
-
-
-const TASKS = [
-  {nameTask: "Кодинг", id: Date.now(), isCoding: false},
-  {nameTask: "Чтение", id: Date.now(), isCoding: false},
-  {nameTask: "Обед", id: Date.now(), isCoding: false},
-  {nameTask: "Спорт", id: Date.now(), isCoding: false},
-  {nameTask: "Ужин", id: Date.now(), isCoding: false},
-  {nameTask: "Прогулка", id: Date.now(), isCoding: false}
-]
-
 ReactDOM.render(
   <React.StrictMode>
-    <TodoCard tasks={TASKS} />
+    <TodoCard />
   </React.StrictMode>,
   document.getElementById('root')
 );
